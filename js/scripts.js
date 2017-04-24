803417391a6f71ea88b927ab31c31cec8096578e
Alyssa Moody and phat nguyen <alyssa@>
2017-04-24 13:04:55 -0700

    added formulas to js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,11 @@ var liters = function(gallons) {
 	return (gallons * 3.78541);
 };
 
-alert(gallons * 3.78541);
+alert((gallons * 3.78541) + " liters");
+
+var teaspoons = parseInt(prompt("How many teaspoons"));
+
+var tablespoons = function(teaspoons) {
+  return (teaspoons / 3);
+};
+alert((teaspoons / 3) + " tablespoons");
